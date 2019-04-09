f4cb50450193d902ead56f38c273e56230c0481d
Blockost <simon.espigole@gmail.com>
2019-04-08 23:45:28 -0400

    config: remove all wrongly committed JS files + add rule in .gitignore

--- src/config/inversify/serviceIdentifiers.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SERVICE_IDENTIFIERS = {
-    UserService: Symbol.for('UserService')
-};
-exports.default = SERVICE_IDENTIFIERS;
