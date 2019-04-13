934a8c3335a45306efb1d6f9093a16b5da304a3d
Blockost <simon.espigole@gmail.com>
2019-04-12 22:54:03 -0400

    fix: unfortunate semicolon in service indentifiers...

--- src/config/inversify/serviceIdentifiers.ts
+++ src/config/inversify/serviceIdentifiers.ts
@@ -1,7 +1,7 @@
 const SERVICE_IDENTIFIERS = {
   UserService: Symbol.for('UserService'),
   HttpLoggerMiddleware: Symbol.for('HttpLoggerMiddleware'),
-  LoggerService: Symbol.for('LoggerService');
+  LoggerService: Symbol.for('LoggerService')
 };
 
 export default SERVICE_IDENTIFIERS;
